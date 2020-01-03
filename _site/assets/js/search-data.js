c8b5c5960e2cf5e2a8dbe88de110629ff6a8066c
vijaypolimeru <vijaypolimeru@gmail.com>
2020-01-03 17:21:53 +0530

    site

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,20 +2,20 @@ var store = [{
         "title": "Edge Case: Nested and Mixed Lists",
         "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that lists within lists do not break the ordered list numbering order and list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
         "tags": ["content","css","edge case","lists","markup"],
-        "url": "http://localhost:4000/randomposts/edge%20case/2009/05/15/edge-case-nested-and-mixed-lists.html"
+        "url": "/randomposts/edge%20case/2009/05/15/edge-case-nested-and-mixed-lists.html"
       },{
         "title": "Chocolate Chip Cookies",
         "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/randomposts/recipes/chocolate-chip-cookies/"
+        "url": "/randomposts/recipes/chocolate-chip-cookies/"
       },{
         "title": "Oatmeal Cookies",
         "excerpt":"Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts. Ingredients 1 cup butter, softened 1 cup white sugar 1 cup packed brown sugar 2 eggs 1 teaspoon vanilla extract 2 cups all-purpose flour 1...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/randomposts/recipes/oatmeal-cookies/"
+        "url": "/randomposts/recipes/oatmeal-cookies/"
       },{
         "title": "Peanut Butter Cookies",
         "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/randomposts/recipes/peanut-butter-cookies/"
+        "url": "/randomposts/recipes/peanut-butter-cookies/"
       }]
